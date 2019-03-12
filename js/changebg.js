96d4daac3214251a2e58af755266068d3d7d6911
zhouie <nanzhouie@gmail.com>
2019-03-12 12:48:36 +0800

    图床失效

--- js/changebg.js
+++ js/changebg.js
@@ -3,27 +3,27 @@
 //实现在指定图片集中随机替换背景图片
 
 var imgArr=[
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-1.jpg", 
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-2.jpg", 
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-3.jpg", 
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-4.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-5.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-6.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-7.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-8.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-9.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-10.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-11.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-12.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-13.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-14.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-15.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-16.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-17.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-18.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-19.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-20.jpg",
-    "http://pb37tp5ix.bkt.clouddn.com/img/bg/background-cover-21.jpg"
+    "https://i.loli.net/2019/03/12/5c87360351427.jpg",
+    "https://i.loli.net/2019/03/12/5c873603b8741.jpg",
+    "https://i.loli.net/2019/03/12/5c873603ce3b7.jpg",
+    "https://i.loli.net/2019/03/12/5c873603cfa76.jpg", 
+    "https://i.loli.net/2019/03/12/5c873603d064a.jpg",
+    "https://i.loli.net/2019/03/12/5c873603d46fe.jpg",
+    "https://i.loli.net/2019/03/12/5c873603d7715.jpg",
+    "https://i.loli.net/2019/03/12/5c873603da0f2.jpg",
+    "https://i.loli.net/2019/03/12/5c873603f3157.jpg",
+    "https://i.loli.net/2019/03/12/5c87369c934eb.jpg",
+    "https://i.loli.net/2019/03/12/5c87369c9f796.jpg",
+    "https://i.loli.net/2019/03/12/5c87369ca6808.jpg",
+    "https://i.loli.net/2019/03/12/5c87369cb2bcf.jpg",
+    "https://i.loli.net/2019/03/12/5c87369cd5f1a.jpg",
+    "https://i.loli.net/2019/03/12/5c87369d0b034.jpg",
+    "https://i.loli.net/2019/03/12/5c87369d3417b.jpg",
+    "https://i.loli.net/2019/03/12/5c87369d7dc30.jpg",
+    "https://i.loli.net/2019/03/12/5c87369d8a1f0.jpg",
+    "https://i.loli.net/2019/03/12/5c87369d998ac.jpg",
+    "https://i.loli.net/2019/03/12/5c8736fb4fcf5.jpg",
+    "https://i.loli.net/2019/03/12/5c8736fb5089f.jpg"
 ]; 
  
 
